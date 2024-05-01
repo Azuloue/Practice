6c67c8514a55f232c128031ca249b0b6d99b7bed
Azuloue <ichigomaccha61224@gmail.com>
2024-05-01 21:50:59 +0900

    ブログ投稿一覧の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,14 +8,16 @@ use App\Models\Post;
 class PostController extends Controller
 {//use宣言は外部にあるクラスをPostController内にインポートできる。
 //この場合、App\Models内のPostクラスをインポートしている。
-/**
- * Post一覧を表示する
- *
- * @param Post Poseモデル
- * @return array Postモデルリスト
- */
+
+//Post一覧を表示する
+ 
+ //@param Post Poseモデル
+ //@return array Postモデルリスト
+
 public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
 {
-    return $post->get();//$postの中身を戻り値にする。
+    return view('posts.index')->with(['posts' => $post->get()]);
+    //blade内で使う変数'posts'と設定。'posts'の中身にgetを使いインスタンス化した$postを代入。
+    //$postの中身を戻り値にする。
 }
 }
