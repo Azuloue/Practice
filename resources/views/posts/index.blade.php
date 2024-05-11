684714bbc96f6f32f7d2c4b83724a31792d42d25
Azuloue <ichigomaccha61224@gmail.com>
2024-05-11 21:30:40 +0900

    投稿の表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,14 +7,19 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
+         <h1>Blog Name</h1>
         <div class='posts'>
+            
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
+
                 </div>
-            @endforeach
+            @endforeach        
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
